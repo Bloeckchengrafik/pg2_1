044d0226b6bf108fd3861c442be51dfef331e875
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-04-23 23:36:29 +0200

    feat: devenv

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.GREET = "pg2-1";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.nlohmann_json
+  ];
+
+  languages.cplusplus.enable = true;
+
+  scripts.run.exec = ''
+    mkdir -p build
+    cd build
+    cmake ..
+    cmake --build .
+    cd ..
+    ./build/pg2_1
+  '';
+}
